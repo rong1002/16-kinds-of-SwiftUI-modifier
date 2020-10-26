f84aa86ed857e0ac782e860a703df1a70e5b4927
rong1002 <borong1002@gmail.com>
2020-10-26 18:11:18 +0800

    Initial Commit

---
+++ 16 kinds of SwiftUI modifier/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  16 kinds of SwiftUI modifier
+//
+//  Created by Lin Bo Rong on 2020/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
